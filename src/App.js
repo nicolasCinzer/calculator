2f1536f01dedb6f42a601b087c40095ea71510ca
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-06-28 21:53:08 -0300

    Setting logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import logoFCC from "./images/freecodecamp-logo.png";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="logoContainer">
+        <img src={logoFCC} className="logo" alt="Logo" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
